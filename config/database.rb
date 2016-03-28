3751388401ac1b0e3dced570da5aafb92d475f42
marinacor1 <marinaspam1234@gmail.com>
2016-03-28 14:44:50 -0600

    adding comments

--- config/database.rb
+++ config/database.rb
@@ -1,9 +1,11 @@
 # set the database based on the current environment
 #creates connection between active record and database
-#makes string with database name. 
+#makes string with database name. name should match app controller file.
+#default is developer
 database_name = "film-file-#{FilmFile.environment}"
 
 # connect ActiveRecord with the current database
+#TODO what is an adapter 
 ActiveRecord::Base.establish_connection(
   :adapter  => "sqlite3",
   :database => "db/#{database_name}",
