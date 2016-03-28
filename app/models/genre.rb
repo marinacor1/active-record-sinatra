f3406a7fa3bd47b7036d54dcd78f0bbf60670939
marinacor1 <marinaspam1234@gmail.com>
2016-03-28 16:20:27 -0600

    adding comments

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ActiveRecord::Base
   has_many(:films)
-  
-
 end
+
+#it allows you to go from app to database, find all films that have
+#its foreign key and return them into table
+#allows the collection
