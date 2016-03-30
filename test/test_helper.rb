8137385bb2fb120283cca4a9d72c7b3cff354a16
marinacor1 <marinaspam1234@gmail.com>
2016-03-30 09:13:22 -0600

    adding comements

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,8 @@ require 'capybara'
 
 require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+#truncation - runs truncation command in SQL deletes info and keeps table structure
+#deletion - will delete table
+#transaction - begin transaction, won't delete anything. unpredictable
 
 Capybara.app = FilmFile
